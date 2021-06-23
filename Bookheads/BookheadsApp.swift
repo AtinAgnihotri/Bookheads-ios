34be98c32501c76767163213b1d16f84f3181b58
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-23 22:20:31 +0530

    Initial Commit

---
+++ Bookheads/BookheadsApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookheadsApp.swift
+//  Bookheads
+//
+//  Created by Atin Agnihotri on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BookheadsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
